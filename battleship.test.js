707cc580f381007c269f71beea82e6deed6af9dd
Zehan <zehan.kareem@gmail.com>
2025-10-21 13:32:16 +0600

    feat(iamzehan): Added tests Ship class methods

--- battleship.test.js
+++ battleship.test.js
@@ -1,2 +1,22 @@
 import {Ship} from './index';
 
+let myShip = null;
+beforeEach(()=> {
+    myShip = new Ship(3);
+})
+
+test("Create Ship", ()=> {
+    expect(myShip).toEqual({length:3, hitsTaken:0})
+})
+
+test("Hit Ship", ()=> {
+    myShip.hit();
+    expect(myShip).toEqual({length:3, hitsTaken: 1})
+})
+
+test("Sink Ship", ()=> {
+    while(!myShip.isSunk()){
+        myShip.hit();
+    }
+    expect(myShip).toEqual({length:3, hitsTaken: 3})
+})
