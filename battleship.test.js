fae39ac843a19e8b5d4ec106ddaf33009b1b1f01
Zehan <zehan.kareem@gmail.com>
2025-10-22 16:36:24 +0600

    update(iamzehan): Updated some tests

--- battleship.test.js
+++ battleship.test.js
@@ -1,22 +1,51 @@
-import {Ship} from './index';
+import { Ship, GameBoard } from "./index";
 
 let myShip = null;
-beforeEach(()=> {
+
+describe("Ship class operations", () => {
+  beforeEach(() => {
     myShip = new Ship(3);
-})
+  });
+  afterEach(() => {
+    myShip = null;
+  });
+  test("Create Ship", () => {
+    expect(myShip).toEqual({ length: 3, hitsTaken: 0 });
+  });
 
-test("Create Ship", ()=> {
-    expect(myShip).toEqual({length:3, hitsTaken:0})
-})
-
-test("Hit Ship", ()=> {
+  test("Hit Ship", () => {
     myShip.hit();
-    expect(myShip).toEqual({length:3, hitsTaken: 1})
-})
+    expect(myShip).toEqual({ length: 3, hitsTaken: 1 });
+  });
 
-test("Sink Ship", ()=> {
-    while(!myShip.isSunk()){
-        myShip.hit();
+  test("Sink Ship", () => {
+    while (!myShip.isSunk()) {
+      myShip.hit();
     }
-    expect(myShip).toEqual({length:3, hitsTaken: 3})
-})
+    expect(myShip).toEqual({ length: 3, hitsTaken: 3 });
+  });
+});
+
+describe("Gameboard operations", () => {
+  beforeEach(() => {
+    gameBoard = new GameBoard();
+    
+  });
+  afterEach(() => {
+    myShip = null;
+  });
+
+  test("Deploy", () => {
+    myShip = new Ship(3);
+    gameBoard.deploy(myShip, [0,0], true);
+    expect(gameBoard.ships[0]).toEqual(myShip);
+    expect(gameBoard.coordinates).toEqual([[[0,0],[0,1],[0,2]]])
+  });
+  test("Deploy auto", () => {
+    myShip = new Ship(3);
+    gameBoard.deploy(myShip, [0,9]);
+    expect(gameBoard.ships[0]).toEqual(myShip);
+    expect(gameBoard.coordinates).toEqual([[[0,9],[1,9],[2,9]]])
+  });
+
+});
--- index.js
+++ index.js
@@ -1,39 +1,54 @@
 export class Ship {
-    constructor(length){
-        this.length = length;
-        this.hitsTaken = 0;
-    }
-    hit(){
-        this.hitsTaken++;
-    }
-    isSunk(){
-        return this.length === this.hitsTaken;
-    }
+  constructor(length) {
+    this.length = length;
+    this.hitsTaken = 0;
+  }
+  hit() {
+    this.hitsTaken++;
+  }
+  isSunk() {
+    return this.length === this.hitsTaken;
+  }
 }
 
-export class GameBoard{
-    constructor(length, coordinates){
-        this.ship = new Ship(length);
-        this.coordinates = coordinates;
-        this.deploy();
-    }
+export class GameBoard {
+  constructor() {
+    this.ships = [];
+    this.coordinates = [];
+  }
 
-    deploy(long=false){
-        const [row, columns] = this.coordinates;
-        const occupy = [];
-        if (long){
-            let i = 0;
-            while(i<this.ship.length){
-                occupy.push(row, columns+i);
-                i++;
-            }
-        }
-        else{
-            let i = 0;
-            while(i<this.ship.length){
-                occupy.push(row+1, columns);
-                i++;
-            }
-        }
+  #byColumn(row, col, limit){
+    const occupy = [];
+    let i = 0;
+      while (i < limit) {
+        occupy.push([row, col + i]);
+        i++;
+      }
+    return occupy; 
+  }
+
+  #byRow(row, col, limit){
+    const occupy = [];
+    let i = 0;
+      while (i < limit) {
+        occupy.push([row + i, col]);
+        i++;
+      }
+      return occupy;
+  }
+
+  deploy(ship, coordinates, long = false) {
+    const row = coordinates[0];
+    const col = coordinates[1];
+    this.ships.push(ship);
+    if (long === true) {
+      this.coordinates.push(this.#byColumn(row, col, ship.length))
+    } else if (long == false) {
+      this.coordinates.push(this.#byRow(row, col, ship.length))
     }
+  }
 }
+// const gameBoard = new GameBoard();
+// const myShip = new Ship(3);
+// gameBoard.deploy(myShip, [0, 9]);
+// console.log(gameBoard)
