95139d33c12ecfcfadf51633814251f6ce14e61e
Zehan <zehan.kareem@gmail.com>
2025-10-25 11:59:13 +0600

    test(iamzehan): Added test cases for deployment

--- battleship.test.js
+++ battleship.test.js
@@ -10,42 +10,67 @@ describe("Ship class operations", () => {
     myShip = null;
   });
   test("Create Ship", () => {
-    expect(myShip).toEqual({ length: 3, hitsTaken: 0 });
+    expect(myShip).toEqual({ length: 3, hitsTaken: 0, positions: [] });
   });
 
   test("Hit Ship", () => {
     myShip.hit();
-    expect(myShip).toEqual({ length: 3, hitsTaken: 1 });
+    expect(myShip).toEqual({ length: 3, hitsTaken: 1, positions: [] });
   });
 
   test("Sink Ship", () => {
     while (!myShip.isSunk()) {
       myShip.hit();
     }
-    expect(myShip).toEqual({ length: 3, hitsTaken: 3 });
+    expect(myShip).toEqual({ length: 3, hitsTaken: 3, positions: [] });
   });
 });
 
 describe("Gameboard operations", () => {
   beforeEach(() => {
     gameBoard = new GameBoard();
-    
+    myShip = new Ship(3);
   });
   afterEach(() => {
     myShip = null;
   });
 
-  test("Deploy", () => {
-    myShip = new Ship(3);
-    gameBoard.deploy(myShip, [0,0], true);
-    expect(gameBoard.ships[0]).toEqual(myShip);
-    expect(gameBoard.coordinates).toEqual([[[0,0],[0,1],[0,2]]])
-  });
-  test("Deploy auto", () => {
-    myShip = new Ship(3);
-    gameBoard.deploy(myShip, [0,9]);
-    expect(gameBoard.ships[0]).toEqual(myShip);
-    expect(gameBoard.coordinates).toEqual([[[0,9],[1,9],[2,9]]])
+  test("Deploy auto (horizontal)", () => {
+    expect(gameBoard.deploy(myShip, [0, 9])).toBe(false);
+    expect(gameBoard.deploy(myShip, [0, 0])).toBe(true);
   });
 
+  test("Detect collisions (horizontal)", () => {
+    expect(gameBoard.deploy(myShip, [0, 0])).toBe(true);
+    // detect collisions
+    expect(gameBoard.deploy(myShip, [0, 0])).toBe(false);
+    expect(gameBoard.deploy(myShip, [0, 1])).toBe(false);
+    expect(gameBoard.deploy(myShip, [0, 2])).toBe(false);
+    // detect non-collisions
+    expect(gameBoard.deploy(myShip, [0, 3])).toBe(true);
+  });
+
+  test("Deploy auto (vertical)", () => {
+    expect(gameBoard.deploy(myShip, [9, 0], "vertical")).toBe(false);
+    expect(gameBoard.deploy(myShip, [0, 0], "vertical")).toBe(true);
+  });
+
+  test("Detect collisions (vertical)", () => {
+    expect(gameBoard.deploy(myShip, [0, 0], "vertical")).toBe(true);
+    // detect collisions
+    expect(gameBoard.deploy(myShip, [0, 0], "vertical")).toBe(false);
+    expect(gameBoard.deploy(myShip, [1, 0], "vertical")).toBe(false);
+    expect(gameBoard.deploy(myShip, [2, 0], "vertical")).toBe(false);
+    // detect non-collisions
+    expect(gameBoard.deploy(myShip, [0, 1], "vertical")).toBe(true);
+  });
+
+  test("Detect positions (horizontal)",()=> {
+    gameBoard.deploy(myShip, [0, 0]);
+    expect(myShip.positions).toEqual([[0,0],[0,1],[0,2]]);
+  });
+  test("Detect positions (vertical)",()=> {
+    gameBoard.deploy(myShip, [0, 0], "vertical");
+    expect(myShip.positions).toEqual([[0,0],[1,0],[2,0]]);
+  });
 });
