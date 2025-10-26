6cd96c9bdb1dea46e2ace5883d134b9d3b39ba84
Zehan <zehan.kareem@gmail.com>
2025-10-27 04:29:41 +0600

    feat(iamzehan): Added deploy ship UI

--- src/components/Start/index.js
+++ src/components/Start/index.js
@@ -9,35 +9,56 @@ export default class Start {
       .cloneNode(true);
     this.dialog.appendChild(this.gameboard);
     this.cells = this.gameboard.querySelectorAll(".row>div");
-    this.placingShipEvent();
+    this.#placingShipEvent();
   }
 
-  #placeShipPreview(cell, length, align){
-    const [x, y] = cell.className.split("-")[1].split("")
-    if (align==="h" && parseInt(y)+length>10) {
-      return;
+  #deployShipPreview(cell, length, align) {
+    // get the cell numbers
+    const [x, y] = cell.className.split("-")[1].split("");
+    //bounds check
+    if (
+      (align === "h" && parseInt(y) + length > 10) ||
+      (align === "v" && parseInt(x) + length > 10)
+    ) {
+      return null;
     }
-    if( align==="v" && parseInt(x)+length > 10) {
-      return;
-    }
-    for(let i=0; i<length; i++){
-      try{
-      const cellNo = align==='h'?`${x}${parseInt(y)+i}`:`${parseInt(x)+i}${y}`;
+
+    const cells = [];
+    for (let i = 0; i < length; i++) {
+      const cellNo =
+        align === "h" ? `${x}${parseInt(y) + i}` : `${parseInt(x) + i}${y}`;
       const cell = this.dialog.querySelector(`.col-${cellNo}`);
-       cell.classList.toggle("preview")
-      }catch{
+      // collision check
+      if (cell.classList.contains("deploy")) {
         return;
       }
+      cells.push(cell);
     }
+    cells.forEach((c) => {
+      c.classList.toggle("preview");
+    });
+    return cells;
   }
-  placingShipEvent() {
-    this.cells.forEach((cell) => {
-      cell.addEventListener("mouseover", (e)=> {
-        this.#placeShipPreview(cell, 3, "v");
-      });
-      cell.addEventListener("mouseout", ()=>{
-        this.#placeShipPreview(cell, 3, "v");
+
+  #deployShip(cell, length, align) {
+    const cells = this.#deployShipPreview(cell, length, align);
+    return (cells.length>0)?
+    cells.forEach((c) => {
+        c.classList.add("deploy");
       })
+    :null;
+  }
+  #placingShipEvent() {
+    this.cells.forEach((cell) => {
+      cell.addEventListener("mouseover", (e) => {
+        this.#deployShipPreview(cell, 3, "v");
+      });
+      cell.addEventListener("mouseout", () => {
+        this.#deployShipPreview(cell, 3, "v");
+      });
+      cell.addEventListener("click", () => {
+        this.#deployShip(cell, 3, "v");
+      });
     });
   }
 
