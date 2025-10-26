e009e6ba036016f484e2cebcc630c2197ea54c50
Zehan <zehan.kareem@gmail.com>
2025-10-26 23:04:39 +0600

    update(iamzehan): First draft of the interactive UI

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-import {Ship, Player} from "../game_logic";
-import './style.css';
+import { Ship, GameBoard, Player } from "../game_logic";
+import "./style.css";
+
+const cells = document.querySelectorAll("div.attack>div.row>div");
+const comp = new Player();
+
+comp.board.deploy(comp.ships.destroyer, [0, 0]);
+comp.board.deploy(comp.ships.corvette, [5, 9], "vertical");
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    const [x, y] = cell.className.split("");
+    if (comp.board.receiveAttack([parseInt(x), parseInt(y)])) {
+      cell.innerHTML = "<span class='marker'>🔥</span>";
+      cell.classList.add("hit");
+    } else {
+      cell.innerHTML = "<span class='marker'>🌊</span>";
+      cell.classList.add("miss");
+    }
+  });
+});
