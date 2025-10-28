ecd806bcb476f8dacab63743464c793ba8122a49
Zehan <zehan.kareem@gmail.com>
2025-10-29 02:41:37 +0600

    feat(iamzehan): Show deployed ships in player's gameboard

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import { Ship, GameBoard, Player } from "./game_logic";
-import Start from './components/Start'
+import Start from "./components/Start";
 import "./style.css";
 
-document.addEventListener("DOMContentLoaded", ()=> {
-  document.body.appendChild(new Start().load());
+document.addEventListener("DOMContentLoaded", () => {
+  let dialog = new Start();
+  document.body.appendChild(dialog.dialog);
+  dialog.open();
+  dialog.startBtn.addEventListener("click", () => {
+    const gameboard = document.querySelector("div.gameboard>div.player");
+    const rows = dialog.gameboard.querySelectorAll("div.row");
+    console.log(rows);
+    gameboard.innerHTML = "";
+    rows.forEach((row) => {
+      gameboard.appendChild(row);
+    });
+  });
 });
 const cells = document.querySelectorAll("div.attack>div.row>div");
 const comp = new Player();
@@ -13,7 +24,7 @@ comp.board.deploy(comp.ships.corvette, [5, 9], "vertical");
 
 cells.forEach((cell) => {
   cell.addEventListener("click", () => {
-    const [x, y] = cell.className.split("");
+    const [x, y] = cell.className.split("-")[1].split("").map(Number);
     if (comp.board.receiveAttack([parseInt(x), parseInt(y)])) {
       cell.innerHTML = "<span class='marker'>🔥</span>";
       cell.classList.add("hit");
